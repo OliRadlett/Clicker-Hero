caed5998ea692765c242f34ccc07fa41fa4c0c34
Oli Radlett <o.radlett@gmail.com>
2018-03-29 18:41:44 +0100

    Add initial app and server to test POST requests

--- app/app/src/main/java/oli/clicker_hero/MainActivity.java
+++ app/app/src/main/java/oli/clicker_hero/MainActivity.java
@@ -1,44 +1,72 @@
 package oli.clicker_hero;
 
+import android.app.VoiceInteractor;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    public void POST(RequestQueue rq, final String key, final String value) {
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+        String url = "http://192.168.178.87:1234";
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println(response);
             }
-            return false;
-        }
-    };
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error);
+            }
+        }) {
+
+            protected Map<String, String> getParams() {
+
+                Map<String, String> data = new HashMap<String, String>();
+                data.put(key, value);
+                return data;
+
+            }
+
+        };
+
+        rq.add(request);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        final RequestQueue queue = Volley.newRequestQueue(this);
+
+        final Button posttest = findViewById(R.id.posttest);
+
+        posttest.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                POST(queue,"testKey", "testValue");
+
+            }
+
+        });
+
     }
 
 }
---
+++ server/server.js
@@ -0,0 +1,46 @@
+var http = require('http');
+var qs = require("query-string");
+
+function processPost(request, response, callback) {
+    var queryData = "";
+    if(typeof callback !== 'function') return null;
+
+    if(request.method == 'POST') {
+        request.on('data', function(data) {
+            queryData += data;
+            if(queryData.length > 1e6) {
+                queryData = "";
+                response.writeHead(413, {'Content-Type': 'text/plain'}).end();
+                request.connection.destroy();
+            }
+        });
+
+        	request.on('end', function() {
+            request.post = qs.parse(queryData);
+            callback();
+        });
+
+    } else {
+        response.writeHead(405, {'Content-Type': 'text/plain'});
+        response.end();
+    }
+}
+
+http.createServer(function(req, res) {
+
+	res.write("Test");
+	res.end();
+
+	if (req.method == "POST") {
+
+		processPost(req, res, function() {
+
+			console.log(req.post);
+			res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+            res.end();
+
+		})
+
+	}
+
+}).listen(1234, "192.168.178.87");
